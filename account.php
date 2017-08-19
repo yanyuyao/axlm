3a3b3e4cef935fd429ef68b9e91673c2e6143f16
yanyuyao <915981876@qq.com>
2017-08-20 03:00:43 +0800

    推广列表，转账

--- account.php
+++ account.php
@@ -43,13 +43,13 @@ $back_act = '';
 *
 */
 $not_login_arr = array(
-	'default', 'detaillog'
+	'default', 'detaillog','show_expend_users','zhuanzhang'
 );
 /* 余额额支付密码_更改_END_www.cfweb2015.com */
 
 /* 显示页面的action列表 */
 $ui_arr = array(
-	'default', 'detaillog'
+	'default', 'detaillog','show_expend_users','zhuanzhang'
 ); // 代码修改
 
 /* 未登录处理 */
@@ -132,6 +132,8 @@ if(! function_exists($function_name))
 {
 	$function_name = "action_default";
 }
+
+//echo md5('123456');
 call_user_func($function_name);
 /* 路由 */
 
@@ -150,6 +152,11 @@ function action_default ()
 	$pc_user = $db->getRow($sql);
 	$smarty->assign('pc_user',$pc_user);
 	
+	$data = array();
+	getAllUserListByUid($user_id,$data);
+
+	
+	$smarty->assign('userTotal', count($data));
 	
 	$smarty->assign('user_id', $_SESSION['user_id']);
 	$smarty->assign('mydomain', $ecs->url());
@@ -206,26 +213,134 @@ function action_show_expend_users()
 	$user_id = $_SESSION['user_id'];
 	
 	$data = array();
-	$userlist = getAllUserListByUid($user_id,$data);
+	getAllUserListByUid($user_id,$data);
 
-	var_dump($data);
+	//var_dump($data);
 	$smarty->assign('user_id', $_SESSION['user_id']);
+	$smarty->assign('userlist', $data);
+	
+	$smarty->display('user_account.dwt');
+}
+//转账现金币
+function action_zhuanzhang(){
+	
+	// 获取全局变量
+	$_LANG = $GLOBALS['_LANG'];
+	$smarty = $GLOBALS['smarty'];
+	$db = $GLOBALS['db'];
+	$ecs = $GLOBALS['ecs'];
+	$user_id = $_SESSION['user_id'];
+	
+	$xianjinbi = $db->getOne('select account_xianjinbi from '.$ecs->table('pc_user')." where uid = $user_id");
+	$xianjinbi = intval($xianjinbi?$xianjinbi:0);
+	//echo 'select pwd2 from '.$ecs->table('pc_user')." where uid = $user_id";
+	$pwd2 = $db->getOne('select pwd2 from '.$ecs->table('pc_user')." where uid = $user_id");
+	$curstep = isset($_REQUEST['step'])?$_REQUEST['step']:'';
+	$touser = isset($_REQUEST['touser'])?$_REQUEST['touser']:0;
+	$tousername = $db->getOne('select user_name from '.$ecs->table('users')." where user_id = $touser");
+	$smarty->assign('tousername',$tousername);
+	$step = "default";
+	//检查密码
+	if($curstep == 'checkpwd'){
+		$pwd = isset($_REQUEST['pwd2'])?$_REQUEST['pwd2']:'';
+		
+		//echo "<br>";echo md5($pwd);echo "000<br>";echo $pwd2;
+		
+		if(md5($pwd) == $pwd2){
+			//echo 'select user_name from '.$ecs->table('users')." where user_id = $touser";
+			
+			$step = "zhuanzhang";
+		}else{
+			$msg = "密码不匹配，请重新输入！";
+			$smarty->assign("msg",$msg);
+			$step = "default";
+		}
+	}elseif($curstep == 'zhuanzhang'){//执行转账操作
+		$amount = intval(isset($_REQUEST['amount'])?$_REQUEST['amount']:0);
+		if($xianjinbi < $amount && $amount){
+			$msg = "转账金额不能超过您的现金币,并且转账金额不能为0";
+			$smarty->assign("msg",$msg);
+			$step = 'zhuanzhang';
+		}else{
+			zhangzhangLog('account_xianjinbi',$user_id,$touser,$amount);
+			$step = 'success';
+		}
+		
+	}else{
+		$step = "default";
+	}
+	
+	$smarty->assign('step',$step);
+	$smarty->assign('user_id', $_SESSION['user_id']);
+	$smarty->assign('touser', $touser);
 	
 	$smarty->display('user_account.dwt');
 }
 
+/*
+* bizhong : 币种 account_xianjinbi, account_xiaofeibi, account_aixinbi,account_jifenbi, account_jifen,
+* uid : 用户id
+* touid ：转给uid
+* amount : 金额
+* 现在可以转账的只有现金币
+*/
+function zhangzhangLog($bizhong,$from_uid,$to_uid,$amount){
+	
+	$db = $GLOBALS['db'];
+	$ecs = $GLOBALS['ecs'];
+	$time = time();
+	
+	if($bizhong == "account_xianjinbi"){
+		$sql = "insert into ".$ecs->table('pc_zhuanzhang_log')."(uid,from_uid,amount,type,ctime,utime)values('".$from_uid."','".$to_uid."','".$amount."','".$bizhong."','".$time."','".$time."')";
+		$db->query($sql);
+		
+		//更改每个人的账号变化记录
+		save_user_account_log($from_uid,$bizhong,"-",$amount);
+		save_user_account_log($to_uid,$bizhong,"+",$amount);
+		
+	}
+	
+}
+function save_user_account_log($uid,$type,$amount_type,$amount){
+	
+	$db = $GLOBALS['db'];
+	$ecs = $GLOBALS['ecs'];
+	
+	$original_value = 0;
+	$new_value = 0;
+	$adminid = 0;
+	$note = '转账';
+	$ctime = time();
+	
+	$original_value = $db->getOne("select account_xianjinbi from ".$ecs->table('pc_user')." where uid = $uid");
+	$original_value = intval($original_value);
+	if($amount_type == "+"){
+		$new_value = $original_value + intval($amount);
+	}else{
+		$new_value = $original_value - intval($amount);
+	}
+	$new_value = intval($new_value);
+	
+	$sql = "insert into ".$ecs->table('pc_user_account_log')."(uid,type,original_value,change_value,new_value,note,adminid,ctime)values('".$uid."','".$type."','".$original_value."','".$amount_type.$amount."','".$new_value."','".$note."','".$adminid."','".$ctime."')";
+	$db->query($sql);
+	
+	$sql = "update ".$ecs->table('pc_user')." set $type = ".$new_value." where uid = $uid limit 1";
+	$db->query($sql);
+	
+}		
+//获得用户所以下线列表
 function getAllUserListByUid($uid,&$data){
 	$db = $GLOBALS['db'];
 	$ecs = $GLOBALS['ecs'];
 
-	$sql = "select uid , tuijianren_user_id, jiedianren_user_id, leftright from ".$ecs->table('pc_user')." where tuijianren_user_id = ".$uid;
-	
+	$sql = "select uid , tuijianren_user_id, jiedianren_user_id, leftright, u.user_name,u.reg_time , from_unixtime(u.reg_time,'%Y-%m-%d %H-%i-%s') as reg_time_format from ".$ecs->table('pc_user')." pu left join ".$ecs->table('users')." u on pu.uid = u.user_id where tuijianren_user_id = ".$uid;
+	//echo "<br>".$sql;
 	$nextuser = $db->getAll($sql);
 	
 	if($nextuser){
 		foreach($nextuser as $k=>$v){
 			$data[] = $v;
-			getAllUserListByUid($v['uid']);
+			getAllUserListByUid($v['uid'],$data);
 		}
 	}else{
 		return $data;
